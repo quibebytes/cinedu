d2eb218f6592f9928cca555f8cc2f6c7f95ebe3d
quibebytes <quibebytes@gmail.com>
2026-04-25 21:48:09 -0300

    Add Documentario Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Documentario(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    sinopse = models.CharField(max_length=2500)
+    data_publicacao = models.DateField()
+    duracao = models.DurationField()
+    ultimo_segundo = models.DurationField()
+    thumbnail = models.BinaryField()
+
